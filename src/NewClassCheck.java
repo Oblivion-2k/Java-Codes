9108f7f6268bc697d97c785366f5b4f7d5099a85
Mruncanny <arnnab.arnnab@gmail.com>
2026-01-18 12:12:28 +0530

    Added a Check class to check that everything is working properly and the code is being pushed.

---
+++ src/NewClassCheck.java
@@ -0,0 +1,7 @@
+public class NewClassCheck {
+    public static void main(String[] args) {
+        System.out.println("Just Checking.....!");
+        int a = 5;
+        System.out.println(a*a+1);
+    }
+}
